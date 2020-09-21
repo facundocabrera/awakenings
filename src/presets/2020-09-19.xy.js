1802e12bfb1fdeb6958704fe198b7d3924844e5b
Facundo Cabrera <cabrerafacundo@gmail.com>
2020-09-20 21:00:48 -0300

    2020-09-20 sin2 step 6

--- src/presets/2020-09-19.xy.js
+++ src/presets/2020-09-19.xy.js
@@ -3,7 +3,7 @@
 // https://www.quantamagazine.org/when-magic-is-seen-in-twisted-graphene-thats-a-moire-20190620/
 // https://www.quantamagazine.org/how-to-create-art-with-mathematics-20151008/
 
-const steps = 5;
+const steps = 6;
 const freq = t => t * (2 * Math.PI / steps);
 const x = t => Math.cos(freq(t));
 
